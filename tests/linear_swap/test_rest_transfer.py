3b96bc6165ec803442fd978ea3a9d7c22c38cf2a
adlong <3140618@163.com>
2021-12-31 17:35:30 +0800

    add linear swap transfer

---
+++ tests/linear_swap/test_rest_transfer.py
@@ -0,0 +1,23 @@
+import sys
+import unittest
+
+sys.path.append('../..')
+from tests.config import ACCESS_KEY, SECRET_KEY
+from huobi.linear_swap.rest.transfer import Transfer
+from huobi.utils.logger import logger
+
+class TestRestAccount(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.api = Transfer(ACCESS_KEY, SECRET_KEY)
+
+    def test_transfer(self):
+        result = self.api.transfer(
+            {"from": "linear-swap", "to": "spot", "currency": "usdt", "amount": 5, "margin-account": "usdt"})
+        logger.info(result)
+        self.assertEqual(True, result['success'])
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
