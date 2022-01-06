fcc6d2aff3af03f9760091f1a625fbcdd414a7b3
adlong <3140618@163.com>
2022-01-06 16:34:32 +0800

    update test case

--- tests/coin_swap/test_rest_trigger_order.py
+++ tests/coin_swap/test_rest_trigger_order.py
@@ -47,31 +47,37 @@ class TestRestTriggerOrder(unittest.TestCase):
         result = self.api.tpsl_order(
             {"contract_code": "trx-usd", "direction":"sell", "volume":1, "tp_trigger_price":0.065, "tp_order_price":0.065, "tp_order_price_type":"limit"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_tpsl_cancel(self):
         result = self.api.tpsl_cancel(
             {"contract_code": "trx-usd", "order_id":"813734657874460672"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_tpsl_cancel_all(self):
         result = self.api.tpsl_cancel_all(
             {"contract_code": "trx-usd", "direction":"sell"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_get_tpsl_open_orders(self):
         result = self.api.get_tpsl_open_orders(
             {"contract_code": "trx-usd"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_get_tpsl_his_orders(self):
         result = self.api.get_tpsl_his_orders(
             {"contract_code": "trx-usd", "status":"0", "create_date":90})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_get_relation_tpsl_order(self):
         result = self.api.get_relation_tpsl_order(
             {"contract_code": "trx-usd", "order_id":""})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
 
 if __name__ == '__main__':
--- tests/linear_swap/test_rest_trigger_order.py
+++ tests/linear_swap/test_rest_trigger_order.py
@@ -77,61 +77,73 @@ class TestRestTriggerOrder(unittest.TestCase):
         result = self.api.isolated_tpsl_order(
             {"contract_code": "eos-usdt", "direction":"sell", "volume":1, "tp_trigger_price":5, "tp_order_price":5, "tp_order_price_type":"limit"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_cross_tpsl_order(self):
         result = self.api.cross_tpsl_order(
             {"contract_code": "eos-usdt", "direction":"sell", "volume":1, "sl_trigger_price":2, "sl_order_price":2, "sl_order_price_type":"limit"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_isolated_tpsl_cancel(self):
         result = self.api.isolated_tpsl_cancel(
             {"contract_code": "eos-usdt", "order_id":"813734657874460672"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_cross_tpsl_cancel(self):
         result = self.api.cross_tpsl_cancel(
             {"contract_code": "eos-usdt", "order_id":"813735137312677889"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_isolated_tpsl_cancel_all(self):
         result = self.api.isolated_tpsl_cancel_all(
             {"contract_code": "eos-usdt", "direction":"sell"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_cross_tpsl_cancel_all(self):
         result = self.api.cross_tpsl_cancel_all(
             {"contract_code": "eos-usdt", "direction":"sell"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_isolated_get_tpsl_open_orders(self):
         result = self.api.isolated_get_tpsl_open_orders(
             {"contract_code": "eos-usdt"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_cross_get_tpsl_open_orders(self):
         result = self.api.cross_get_tpsl_open_orders(
             {"contract_code": "eos-usdt"})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_isolated_get_tpsl_his_orders(self):
         result = self.api.isolated_get_tpsl_his_orders(
             {"contract_code": "eos-usdt", "status":"0", "create_date":90})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_cross_get_tpsl_his_orders(self):
         result = self.api.cross_get_tpsl_his_orders(
             {"contract_code": "eos-usdt", "status":"0", "create_date":90})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_isolated_get_relation_tpsl_order(self):
         result = self.api.isolated_get_relation_tpsl_order(
             {"contract_code": "eos-usdt", "order_id":""})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
     def test_cross_get_relation_tpsl_order(self):
         result = self.api.cross_get_relation_tpsl_order(
             {"contract_code": "eos-usdt", "order_id":""})
         logger.info(result)
+        self.assertEqual('ok', result['status'])
 
 
 if __name__ == '__main__':
