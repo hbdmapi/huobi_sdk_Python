3f4414a70bcc621c9b501116fcbeba70a732556d
adlong <3140618@163.com>
2022-01-25 17:26:49 +0800

    update for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="huobipy",
-    version="1.0.0",
+    version="1.0.1",
     author="adlong",
     author_email="3140618@163.com",
     description="huobi api sdk",
